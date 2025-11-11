7e4136b7f0eb0edecd8ebfc995d37ac4696edeca
wjt <1412499544@qq.com>
2025-11-11 12:57:02 +0800

    2025.11.11 王景涛
    添加了Colourful Hierarchy Category GameObject
    添加了dotween
     添加了addressables
    添加了Unitask
    添加脚本singleton，events

---
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T> 
+{
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance != null)
+            Destroy(gameObject);
+        else
+            Instance = (T)this;  
+    }
+
+    public static bool IsInitialized => Instance != null;
+
+    protected virtual void OnDestory()
+    {
+        if (Instance == this)
+            Instance = null;
+    }
+}
